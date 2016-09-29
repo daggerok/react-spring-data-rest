efe8209439672d9066883776f21a44935758d040
Maksim Kostromin <daggerok@gmail.com>
2016-09-29 06:04:08 +0300

    fix update component after user was removed

--- client/src/components/admin/User.js
+++ client/src/components/admin/User.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import rest from 'rest';
 
-const rmClient = (id) => rest({
-  method: 'DELETE',
-  path: `/api/users/${id}`
-});
-const rm = (id) => rmClient(id).then(
-  response => console.log(`user with id ${id} removed`),
-  error => console.error('error', error)
-);
-
-export const User = ({ user, updated }) => (
+export const User = ({ user, rmUserById }) => (
   <tr key={user.id} class="row">
-    <td class="col-sm-3"><button onClick={() => {
-      rm(user.id);
-      updated();
-    }}>X</button> {user.id}</td>
+    <td class="col-sm-3"><button onClick={() => rmUserById(user.id)}>X</button> {user.id}</td>
     <td class="col-sm-3">{user.username}</td>
     <td class="col-sm-6">{user.roles.map((role, i) => <span key={i}>{role}{i === user.roles.length - 1 ? '' : ', '}</span>)}</td>
   </tr>
--- client/src/components/admin/Users.js
+++ client/src/components/admin/Users.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import rest from 'rest';
-import { User } from './User';
-import { UsersConverter } from '../../utils/converters/UsersConverter';
+import React from "react";
+import rest from "rest";
+import {User} from "./User";
+import {UsersConverter} from "../../utils/converters/UsersConverter";
+
+const uri = '/api/users';
+const getUsersClient = rest(uri);
+const rmUserClient = (id) => rest({
+  method: 'DELETE',
+  path: `${uri}/${id}`
+});
 
 export class Users extends React.Component {
   constructor() {
     super();
     this.state = { users: [] };
-    this.client = rest('/api/users');
-    this.updated = this.updated.bind(this);
+    this.rmUserById = this.rmUserById.bind(this);
   }
 
   componentDidMount() {
-    this.client.then(response => {
-      const { entity } = response;
+    getUsersClient.then(response => {
+      const {entity} = response;
       const hateoas = JSON.parse(entity);
       const users = new UsersConverter().users(hateoas);
+
       this.setState({ users });
     });
   }
 
-  componentDidUpdate() {
-    console.log(`updated`)
-  }
-
-  updated() {
-    this.forceUpdate();
+  rmUserById(id) {
+    rmUserClient(id).then(
+      response => this.setState({ users: [ ...this.state.users.filter(user => user.id != id) ] }),
+      error => console.error('error', error)
+    );
   }
 
   render() {
     return (
       <tbody class="table-striped">
-      {this.state.users.map((user, i) => <User key={i} updated={this.updated} user={user}/>)}
+        {this.state.users.map((user, i) => <User key={i} user={user} rmUserById={this.rmUserById}/>)}
       </tbody>
     );
   }
